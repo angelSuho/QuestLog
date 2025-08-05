7cd6279ff431f713563085632e437dcafd7211e9
angelSuho <angelsuho0303@gmail.com>
2025-08-05 16:18:51 +0900

    project init

---
+++ src/test/java/app/suho/questlog/QuestlogApplicationTests.java
@@ -0,0 +1,13 @@
+package app.suho.questlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
